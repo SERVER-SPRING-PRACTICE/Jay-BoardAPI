40b7baed68da5d9d66f1f054091a82bb550b2282
2Re-play <wogusduddnjs@gmail.com>
2020-03-30 16:01:52 +0900

    [Post] 로그인 controller 기능 구현 완료

--- src/main/java/com/cadi/boardapi/controller/UserController.java
+++ src/main/java/com/cadi/boardapi/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.cadi.boardapi.controller;
 
 import com.cadi.boardapi.model.DefaultRes;
+import com.cadi.boardapi.model.SignInReq;
 import com.cadi.boardapi.model.SignUpReq;
 import com.cadi.boardapi.service.UserService;
 import com.cadi.boardapi.util.ResponseMessage;
@@ -33,6 +34,16 @@ public class UserController {
     public ResponseEntity signUp (@RequestBody SignUpReq signUpReq) {
         try {
             return new ResponseEntity(userService.signUp(signUpReq), HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity(FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("signin")
+    public ResponseEntity signIn (@RequestBody SignInReq signInReq) {
+        try {
+            return new ResponseEntity(userService.singIn(signInReq), HttpStatus.OK);
         } catch (Exception e) {
             log.error(e.getMessage());
             return new ResponseEntity(FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
