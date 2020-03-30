296a6baf05b9ee69a6d14ba3f3416327dd4ab06b
2Re-play <wogusduddnjs@gmail.com>
2020-03-30 14:25:18 +0900

    first commit

---
+++ src/main/java/com/cadi/boardapi/BoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.cadi.boardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardApiApplication.class, args);
+    }
+
+}
