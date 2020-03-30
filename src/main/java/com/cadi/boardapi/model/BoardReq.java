85a364ec93db8faf2a419c051711ab160550b021
2Re-play <wogusduddnjs@gmail.com>
2020-03-30 18:11:34 +0900

    [Post] 게시판 글 쓰기  model 기능 구현 완료

--- src/main/java/com/cadi/boardapi/model/BoardReq.java
+++ src/main/java/com/cadi/boardapi/model/BoardReq.java
@@ -1,4 +1,17 @@
 package com.cadi.boardapi.model;
 
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
 public class BoardReq {
+
+    private String title;
+    private String content;
+
+    private List<MultipartFile> images;
+    private List<String> imagesUrl;
+
 }
