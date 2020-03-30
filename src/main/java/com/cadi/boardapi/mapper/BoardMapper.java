543fa566757b04ae8d1a6843ae9c9b9d8eea64ab
2Re-play <wogusduddnjs@gmail.com>
2020-03-30 23:24:08 +0900

    [GET] 게시판 리스트 가져오기 mapper 기능 구현 완료

--- src/main/java/com/cadi/boardapi/mapper/BoardMapper.java
+++ src/main/java/com/cadi/boardapi/mapper/BoardMapper.java
@@ -1,14 +1,21 @@
 package com.cadi.boardapi.mapper;
 
-import com.cadi.boardapi.model.BoardReq;
+import com.cadi.boardapi.dto.Board;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface BoardMapper {
 
-    @Insert("INSERT INTO board(title, content, board_image1) VALUES (#{title},#{content},#{image})")
+    @Insert("INSERT INTO board(title, content, board_image) VALUES (#{title},#{content},#{image})")
     void postBoard(@Param("title") String title, @Param("content") String content, @Param("image") String image);
+
+    @Select("SELECT title, content, board_image FROM board")
+    List<Board> getBoardList();
+
 
 }
