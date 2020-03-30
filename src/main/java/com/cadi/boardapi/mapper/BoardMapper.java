a007a9d2d6763031a42b754673bbd7c2c2d41e16
2Re-play <wogusduddnjs@gmail.com>
2020-03-30 18:07:44 +0900

    [Post] 게시판 글 쓰기  mapper 기능 구현 완료

--- src/main/java/com/cadi/boardapi/mapper/BoardMapper.java
+++ src/main/java/com/cadi/boardapi/mapper/BoardMapper.java
@@ -1,4 +1,14 @@
 package com.cadi.boardapi.mapper;
 
+import com.cadi.boardapi.model.BoardReq;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
 public interface BoardMapper {
+
+    @Insert("INSERT INTO board(title, content, board_image1) VALUES (#{title},#{content},#{image})")
+    void postBoard(@Param("title") String title, @Param("content") String content, @Param("image") String image);
+
 }
